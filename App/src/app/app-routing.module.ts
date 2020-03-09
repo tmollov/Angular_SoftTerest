40104a242a763fabc68cedf398737920ea9f6c83
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-09 22:31:36 +0200

    Updated App component

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
-import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NotificationsComponent } from './core/notifications/notifications.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CreateComponent } from './idea/create/create.component';
+import { DetailComponent } from './idea/detail/detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'dashboard',
+    pathMatch: 'full',
+    component: DashboardComponent
+  },
+  {
+    path: 'idea/create',
+    pathMatch: 'full',
+    component: CreateComponent
+  },
+  {
+    path: 'idea/detail',
+    pathMatch: 'full',
+    component: DetailComponent
+  },
+  {
+    path: '**',
+    component: NotfoundComponent
+  }
+]
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutingModule = RouterModule.forRoot(routes);
--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { UserModule } from './user/user.module';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IdeaModule } from './idea/idea.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    UserModule,
+    IdeaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
