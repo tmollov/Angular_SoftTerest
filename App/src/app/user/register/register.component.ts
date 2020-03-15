fd70b1b243f59401f77c6e7c479d72f0128a389c
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-15 22:07:00 +0200

    Updated Register component

--- App/src/app/user/register/register.component.ts
+++ App/src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForum = new FormGroup({
+    username: new FormControl(),
+    password: new FormControl(),
+    repassword: new FormControl()
+  });
 
-  ngOnInit() {
+  loading = false;
+  returnUrl: string;
+
+  constructor(public authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router) {
+    if (this.authService.activeUser) {
+      this.router.navigate(['/']);
+    }
   }
 
+  ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
+  }
+
+  get f() {
+    return this.registerForum.controls;
+  }
+
+  onSubmit() {
+    // stop if form is invalid
+    if (this.registerForum.invalid) {
+      return;
+    }
+    this.loading = true;
+
+    //TODO: remove loading similations
+    setTimeout(() => {
+      this.authService
+        .signup(this.f.username.value, this.f.password.value)
+        .then((data) => {
+          console.log(data);
+          this.loading = false;
+          this.router.navigate(['/']);
+        })
+    }, 1500)
+  }
 }
