cf34ab7dff5262aea897a907e4121f02e670a9b5
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-15 22:05:34 +0200

    Updated Create component

--- App/src/app/idea/create/create.component.ts
+++ App/src/app/idea/create/create.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { DataService } from 'src/app/services/data.service';
+import { Router } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +10,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
-
   ngOnInit() {
   }
 
+  createForm: FormGroup;
+  notificationMessage = "";
+  imageUrlPattern = 'https*:\/\/.*';
+
+  loading = false;
+  constructor(private dataService: DataService,
+    private router: Router,
+    private fb: FormBuilder) {
+    this.createForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(6)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+      imageURL: ['', [Validators.required, Validators.pattern(this.imageUrlPattern)]]
+    });
+  }
+
+  get f() {
+    return this.createForm.controls;
+  }
+
+  showErrors() {
+    console.log(this.createForm.controls.imageURL.errors);
+
+  }
+
+  get https(): boolean {
+    return this.createForm.controls.imageURL.errors.pattern.actualValue.startsWith('https://');
+  }
+
+  get http(): boolean {
+    return this.createForm.controls.imageURL.errors.pattern.actualValue.startsWith('http://');
+  }
+
+  onSubmit() {
+    this.loading = true;
+
+    this.dataService
+      .addIdea(this.f.title.value,
+        this.f.description.value,
+        this.f.imageURL.value)
+      .then((data) => {
+        console.log(data);
+        this.createForm.reset();
+        this.loading = false;
+
+        this.notificationMessage = "Your idea was successfully added!"
+        setTimeout(() => {
+          this.notificationMessage = null;
+        }, 3000);
+      });
+  }
 }
