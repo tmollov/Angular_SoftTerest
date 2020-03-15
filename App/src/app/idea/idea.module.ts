5b0b5c94c9ac94ce1b6c94d94c5ec0eb3858c7f6
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-15 22:02:35 +0200

    Update idea.module.ts

--- App/src/app/idea/idea.module.ts
+++ App/src/app/idea/idea.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateComponent } from './create/create.component';
 import { DetailComponent } from './detail/detail.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from '../core/core.module';
 
 
 
 @NgModule({
   declarations: [CreateComponent, DetailComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    CoreModule
   ]
 })
 export class IdeaModule { }
