625ce466ea59ba7fc6f3288bd907851472bb84c7
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-15 22:02:32 +0200

    Update user.module.ts

--- App/src/app/user/user.module.ts
+++ App/src/app/user/user.module.ts
@@ -4,6 +4,9 @@ import { ProfileComponent } from './profile/profile.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserRoutingModule } from './user-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from '../core/core.module';
+import { LoaderComponent } from '../core/loader/loader.component';
 
 
 
@@ -11,7 +14,9 @@ import { UserRoutingModule } from './user-routing.module';
   declarations: [ProfileComponent, LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    ReactiveFormsModule,
+    CoreModule
   ]
 })
 export class UserModule { }
