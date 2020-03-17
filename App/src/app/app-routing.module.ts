b37e4377d5350587f57be7664ec0cfb72a9402d0
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-17 20:41:07 +0200

    Update app-routing.module.ts

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NotfoundComponent } from './core/notfound/notfound.component';
 import { DashboardComponent } from './home/dashboard/dashboard.component';
 import { CreateComponent } from './idea/create/create.component';
 import { DetailComponent } from './idea/detail/detail.component';
+import { AuthGuard } from './guards/auth.guard';
 
 
 const routes: Routes = [
@@ -11,21 +12,6 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: HomeComponent
-  },
-  {
-    path: 'dashboard',
-    pathMatch: 'full',
-    component: DashboardComponent
-  },
-  {
-    path: 'idea/create',
-    pathMatch: 'full',
-    component: CreateComponent
-  },
-  {
-    path: 'idea/detail/:id',
-    pathMatch: 'full',
-    component: DetailComponent
   },
   {
     path: '**',
