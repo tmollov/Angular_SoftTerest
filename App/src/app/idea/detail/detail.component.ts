1bcc0958fa2ca09fd5c3909b4f58d2fb31d2cf9a
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-03-21 20:59:12 +0200

    Updated Idea/Detail component

--- App/src/app/idea/detail/detail.component.ts
+++ App/src/app/idea/detail/detail.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
-import { Idea } from 'src/app/models/Idea';
-import { DataService } from 'src/app/services/data.service';
-import { AuthService } from 'src/app/services/auth.service';
+import { finalize, map } from 'rxjs/operators';
+import { Idea } from 'src/app/shared/models/Idea';
+import { DataService } from 'src/app/core/services/data.service';
+import { AuthService } from 'src/app/core/services/auth.service';
 
 @Component({
   selector: 'app-detail',
@@ -14,49 +14,60 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class DetailComponent implements OnInit {
 
-  commentform : FormGroup;
+  commentform: FormGroup;
+
   ideaDetails$: Observable<Idea>;
+  ideaModel: Idea;
+  ideaDescriptionList: any[];
+  editLink: string;
+  ideaLikes = 0;
+
   isIdeaLoaded: boolean = false;
   notificationMessage = "Loading idea...";
-  ideaLikes = 0;
+
   constructor(private router: Router,
     private route: ActivatedRoute,
     private dataService: DataService,
     private authService: AuthService,
     private fb: FormBuilder) {
-      this.commentform = this.fb.group({
-        newComment: ['']
-      });
-   }
-
-   ngOnInit(){
-    this.ideaDetails$ = this.dataService.getIdeaById$(this.route.snapshot.params.id)
-    .pipe(finalize(()=>{
-      this.isIdeaLoaded = true;
-    }));
-   }
-
-  IsUserPublisher(id:string): boolean {
-    let res = this.authService.activeUser._id == id    
-    return res
-  }
-
-  get IfThereIsUser(){    
-    return this.authService.activeUser;
-  }
-
-  deleteIdea(ideaID: string){
-    this.dataService.removeIdeaById(ideaID)
-    .then(()=>{
-      this.router.navigate(["/"]);
+    this.commentform = this.fb.group({
+      newComment: ['']
     });
   }
 
-  likeIdea(idea:Idea) {
-    let res = this.dataService.likeIdea(idea,this.authService.activeUser._id)
+  ngOnInit() {
+    this.ideaDetails$ = this.dataService.getIdeaById$(this.route.snapshot.params.id)
+      .pipe(map((data) => {
+        this.isIdeaLoaded = true;
+        this.editLink = "/idea/edit/" + this.route.snapshot.params.id;
+        this.ideaModel = data;
+        this.ideaDescriptionList = data.description.split("\n");
+        return data;
+      }));
+    this.ideaDetails$.subscribe();
   }
-  commentIdea(idea:Idea) {
-    let res = this.dataService.commentIdea(idea,this.authService.activeUser.username,this.commentform.controls.newComment.value)
+
+  IsUserPublisher(id: string): boolean {
+    let res = this.authService.activeUser._id == id
+    return res
+  }
+
+  get IfThereIsUser() {
+    return this.authService.activeUser;
+  }
+
+  deleteIdea(ideaID: string) {
+    this.dataService.removeIdeaById(ideaID)
+      .then(() => {
+        this.router.navigate(["/"]);
+      });
+  }
+
+  likeIdea(idea: Idea) {
+    let res = this.dataService.likeIdea(idea, this.authService.activeUser._id)
+  }
+  commentIdea(idea: Idea) {
+    let res = this.dataService.commentIdea(idea, this.authService.activeUser.username, this.commentform.controls.newComment.value)
     this.commentform.reset();
   }
 }
