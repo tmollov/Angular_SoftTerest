df5c8acff0d9f99f9590fd60f74f0f066a2631f5
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-13 19:31:32 +0300

    Update app.module.ts

--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -3,15 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreModule } from './core/core.module';
-import { UserModule } from './user/user.module';
-import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './home/dashboard/dashboard.component';
-import { IdeaModule } from './idea/idea.module';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-import { HomeModule } from './home/home.module';
-import { AuthService } from './core/services/auth.service';
-import { DataService } from './core/services/data.service';
 import { environment } from 'src/environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -20,13 +14,19 @@ import { ToastrModule } from 'ngx-toastr';
 import { AngularFireAuthModule, AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireModule } from '@angular/fire';
+import { CoreModule } from './components/core/core.module';
+import { UserModule } from './modules/user/user.module';
+import { IdeaModule } from './modules/idea/idea.module';
+import { AuthService } from './services/auth.service';
+import { DataService } from './services/data.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -35,14 +35,13 @@ import { AngularFireModule } from '@angular/fire';
     CoreModule,
     UserModule,
     IdeaModule,
-    HomeModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       autoDismiss: true,
       maxOpened: 2,
       timeOut: 1500,
       easeTime: 200,
-      newestOnTop:false
+      newestOnTop: false
     }),
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
