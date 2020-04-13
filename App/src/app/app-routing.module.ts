f2f27ec5bbc677cbc6eaf085f3546091a4413864
Tundzhay Mollov <graculapsone1998@gmail.com>
2020-04-13 15:30:20 +0300

    Update app-routing.module.ts
    Now dashboard is seperated from home component

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotfoundComponent } from './core/notfound/notfound.component';
 import { DashboardComponent } from './home/dashboard/dashboard.component';
-import { CreateComponent } from './idea/create/create.component';
-import { DetailComponent } from './idea/detail/detail.component';
 import { AuthGuard } from './guards/auth.guard';
 
 
@@ -12,6 +10,11 @@ const routes: Routes = [
     path: '',
     pathMatch: 'full',
     component: HomeComponent
+  },
+  {
+    path: 'dashboard',
+    pathMatch: 'full',
+    component: DashboardComponent
   },
   {
     path: '**',
