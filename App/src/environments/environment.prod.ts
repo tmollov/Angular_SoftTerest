549e26b1ebe20b09e9617cc4aada0fb92b870ae3
Tundzhay Mollov <tmollov@users.noreply.github.com>
2020-09-07 22:36:07 +0300

    Update environment.prod.ts

--- App/src/environments/environment.prod.ts
+++ App/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAf6EKFJJp3Ty8WoxXQDzIGiKD8aTeBwVo",
+    authDomain: "softterest-d4837.firebaseapp.com",
+    databaseURL: "https://softterest-d4837.firebaseio.com",
+    projectId: "softterest-d4837",
+    storageBucket: "softterest-d4837.appspot.com",
+    messagingSenderId: "287048258929",
+    appId: "1:287048258929:web:0357f9981b907a588a92d0",
+    measurementId: "G-50BGSK901L"
+  }
 };
